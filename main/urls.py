676eda4db273482be97da5cc36803f1a7cb24b8b
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-05-12 21:07:50 +0300

    Configurate URLs for create, login users

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,17 @@
 """Base URL Configuration."""
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
+from rest_framework_jwt.views import (obtain_jwt_token,
+                                      verify_jwt_token,
+                                      refresh_jwt_token)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
+
+    url(r'^user/', include('users.urls')),
 ]
